b9431b729ac177479d8160087ee923cfade08c4d
Sharninjak <798909776@qq.com>
2024-04-06 16:48:45 +0800

    202404061648 change back to use strncpy, like this "tokenNode->val = tokenText;" will cause segment dump add hex and oct in lexical.l

--- node.h
+++ node.h
@@ -44,7 +44,7 @@ typedef Node *ptrNode;
 
 ptrNode newNode(int lineNo, NodeType type, char *name, int argc, ...) {
     ptrNode curNode = NULL;
-    // int nameLength = strlen(name) + 1;
+    int nameLength = strlen(name) + 1;
     // 指针自身 = (指针类型*）malloc（sizeof（指针类型）*数据数量）
     curNode = (Node *)malloc(sizeof(Node));
     // curNode = (ptrNode)malloc(sizeof(Node)); // 两个都行
@@ -53,14 +53,14 @@ ptrNode newNode(int lineNo, NodeType type, char *name, int argc, ...) {
 
     // curNode->name = (char*)malloc(sizeof(char) * NAME_LENGTH);
     // curNode->val = (char*)malloc(sizeof(char) * VAL_LENGTH);
-    // curNode->name = (char *)malloc(sizeof(char) * nameLength);
+    curNode->name = (char *)malloc(sizeof(char) * nameLength);
     // assert(curNode->name != NULL);
     // assert(curNode->val != NULL);
 
     curNode->lineNo = lineNo;
     curNode->type = type;
-    curNode->name = name;
-    // strncpy(curNode->name, name, nameLength);
+    // curNode->name = name;
+    strncpy(curNode->name, name, nameLength);
 
     va_list
         vaList; // 存储可变参数列表信息的数据类型，是一个指向栈空间中某个位置的指针
@@ -84,23 +84,19 @@ ptrNode newNode(int lineNo, NodeType type, char *name, int argc, ...) {
 ptrNode newTokenNode(int lineNo, NodeType type, char *tokenName,
                      char *tokenText) {
     ptrNode tokenNode = (ptrNode)malloc(sizeof(Node));
-    int nameLength = strlen(tokenName) + 1;
-    int textLength = strlen(tokenText) + 1;
-
-    assert(tokenNode != NULL);
-
+    // assert(tokenNode != NULL);
     tokenNode->lineNo = lineNo;
     tokenNode->type = type;
-
-    tokenNode->name = (char *)malloc(sizeof(char) * nameLength);
-    tokenNode->val = (char *)malloc(sizeof(char) * textLength);
-
+    // tokenNode->name = tokenName;
+    // tokenNode->val = tokenText;
+    int nameLength = strlen(tokenName) + 1;
+    int textLength = strlen(tokenText) + 1;
     // assert(tokenNode->name != NULL);
     // assert(tokenNode->val != NULL);
-    tokenNode->name = tokenName;
-    tokenNode->val = tokenText;
-    // strncpy(tokenNode->name, tokenName, nameLength);
-    // strncpy(tokenNode->val, tokenText, textLength);
+    tokenNode->name = (char *)malloc(sizeof(char) * nameLength);
+    tokenNode->val = (char *)malloc(sizeof(char) * textLength);
+    strncpy(tokenNode->name, tokenName, nameLength);
+    strncpy(tokenNode->val, tokenText, textLength);
 
     tokenNode->child = NULL;
     tokenNode->next = NULL;
